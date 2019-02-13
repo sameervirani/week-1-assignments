3bd5133f8366f6f075f0ea89a712cb2a7cfa6e26
Sameer <sameer@Sameers-Air.localdomain>
2019-02-13 14:12:25 -0600

    two numbers

---
+++ twonumbers.py
@@ -0,0 +1,7 @@
+no1 = input("enter number One:")
+no2 = input("enter number two:")
+def addition(a,b):
+    result= float(a) + float(b)
+    return result
+result = addition(no1,no2)
+print('And the total is... ' + str(result) + '.')
