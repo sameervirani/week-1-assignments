1fece894f6073a6168b6035d9db3c42428a34078
Sameer <sameer@Sameers-Air.localdomain>
2019-02-13 16:10:10 -0600

    fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,9 +3,9 @@
 #If the input is divisible by 3 and 5 then print "Fizz Buzz".
 
 user1 = int(input("Enter number: "))
-if user1 % 3 == 0:
-    print("Fizz")
-if user1 % 5 == 0:
-    print("Buzz")
 if user1 % 3 == 0 and user1 % 5 == 0:
     print("Fizz Buzz")
+elif user1 % 3 == 0:
+    print("Fizz")
+elif user1 % 5 == 0:
+    print("Buzz")
