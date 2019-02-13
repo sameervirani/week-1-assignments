8f84e1e2c3c59a9a3815301807253897880aba6d
Sameer <sameer@Sameers-Air.localdomain>
2019-02-13 14:31:09 -0600

    fullname.py

---
+++ fullname.py
@@ -0,0 +1,7 @@
+#Write an app which separatly takes first name and last name of the user. Once the name is taken print out the following message:
+#Hello, My name is FirstName, LastName
+
+input1 = input("Enter Your First Name")
+input2 = input("Enter Your Last Name")
+print(input1 + input2)
+print(f"Hello, My name is {input1} {input2}")
